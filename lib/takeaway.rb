8ab73ce171458951143f82a3cead6f067ba14546
jessica stenning <jessicastenning@hotmail.co.uk>
2018-04-28 13:04:14 +0100

    adds list_menu method that prints the menu

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -1,2 +1,12 @@
 class Takeaway
+
+  def initialize(menu:)
+    @menu = menu
+  end
+
+  def list_menu
+    menu.print
+  end
+
+    attr_reader :menu
 end
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -1,3 +1,12 @@
 require './lib/takeaway.rb'
+
 describe Takeaway do
+  subject(:takeaway) {described_class.new(menu: menu) }
+
+let(:menu) { double(:menu, print: list_menu) }
+let(:list_menu) { "pizza £9.99"}
+
+  it 'should print a list of the menu items' do
+    expect(subject.list_menu).to eq(list_menu)
+  end
 end
